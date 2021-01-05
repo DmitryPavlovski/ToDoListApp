1b0df4be67c397ee339f6035d040b772b9767de8
Dmitry Pavlovsky <DmitryPavlovski>
2021-01-05 15:19:06 +0300

    Added Task model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    """Model class Task object"""
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=350)
+    description = models.TextField(verbose_name='Description', null=True)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
