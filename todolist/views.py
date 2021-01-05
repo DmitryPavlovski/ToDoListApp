c03fa19aa567f9f41a11afb3c76178575d3a0133
Dmitry Pavlovsky <DmitryPavlovski>
2021-01-05 18:50:53 +0300

    Added index page and form for add new task

--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import TaskForm
+from .models import Task
 
 # Create your views here.
 
 def main(request):
-    return render(request, "base.html") 
+    return render(request, "base.html")
+
+def index(request):
+    form = TaskForm()
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    tasks = Task.objects.all()
+    return render(request, "index.html", {"task_form": form, "tasks": tasks})
